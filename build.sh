01ab74c7fdae36d0953ac9e10790cb596f456ace
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-18 20:55:06 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
-clang `find ./src/ -name "*.c" -type f` -I./include -I./src/include -I./arch/generic -shared -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
+extraflag=
+if [[ "$1" == "wasm64" ]]; then extraflag=-D__wasm64__; done
+clang `find ./src/ -name "*.c" -type f` -I./include -I./src/include -I./arch/generic -shared -std=c99 -nostdinc $extraflag -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
