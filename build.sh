223c3892a6ae238ab655ee55ee20134a5a546410
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-18 19:55:16 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 set -e
-clang ./src/*.c -I./include -shared -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
+clang `find ./src/ -name "*.c" -type f` -I./include -shared -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
