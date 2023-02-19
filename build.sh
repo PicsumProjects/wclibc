9efc2da31be8b2c6e11d6b3200d4062211c155e3
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-18 19:52:00 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,2 @@
 set -e
-build_file() {
-of=$RANDOM.ll
-clang $2 -I./include -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -emit-llvm -c -S -o $of
-llc -march=$2 -filetype=obj $of -o ${of}.o
-echo ${of}.o
-}
-
-buildlist=
-
-for buildfile in ./src/*.c
-do
-    output=$(build_file $buildfile $1)
-    buildlist=$buildlist $output
-done
-
-wasm-ld --no-entry $buildlist -o wclibc.wasm --strip-all --export-dynamic --import-memory
-
+clang ./src/*.c -I./include -shared -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
