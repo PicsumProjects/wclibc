37cf45ec7bcbe94fdb99f21c58da18c69ee0ed12
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-19 09:35:26 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 set -e
 extraflag=
 if [[ "$1" == "wasm64" ]]; then extraflag=-D__wasm64__; fi
-clang ./src/*/*.c -I./include -I./src/include -I./src/internal -I./arch/wasm -I./arch/generic -std=c99 -nostdlib -Wl,--no-entry $extraflag -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
+clang ./src/*/*.c -I./include -I./src/include -I./src/internal -I./arch/wasm -I./arch/generic -std=c99 -nostdlib -Wl,--no-entry -Wl,--export-all $extraflag -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -o wclibc-$1.wasm
+ls -lah
