aacf0280e8dfceb8b8a972f268d1dcbf512548fd
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2023-02-18 19:27:50 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 build_file() {
 of=$RANDOM.ll
-clang $2 -I./include -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 --emit-llvm -c -S -o $of
+clang $2 -I./include -std=c99 -nostdinc -ffreestanding -fexcess-precision=standard -frounding-math -Wa,--noexecstack -ffunction-sections -fdata-sections -w -Wno-pointer-to-int-cast --target=$1 -emit-llvm -c -S -o $of
 llc -march=$2 -filetype=obj $of -o ${of}.o
 echo ${of}.o
 }
